ae56c667c7c59ba0fbf0539fb4295148c95b54cd
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2021-11-23 10:32:28 +0900

    Update test

--- Tests/ProcessorKitTests/ProcessorKitTests.swift
+++ Tests/ProcessorKitTests/ProcessorKitTests.swift
@@ -2,17 +2,19 @@
     @testable import ProcessorKit
 
     final class ProcessorKitTests: XCTestCase {
-        let usage = CPU.systemUsage()
-        
+       
         func testSystemUsage() {
+            let usage = CPU.systemUsage()
             XCTAssertTrue(usage.system >= 0.0)
         }
         
         func testUserUsage() {
+            let usage = CPU.systemUsage()
             XCTAssertTrue(usage.user >= 0.0)
         }
 
         func testIdleUsage() {
+            let usage = CPU.systemUsage()
             XCTAssertTrue(usage.idle >= 0.0)
         }
 
